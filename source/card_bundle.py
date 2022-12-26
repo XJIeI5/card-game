0a64f898e4362b1eb13c2929793248e59829b56c
Horlou <egorchernov37@yandex.ru>
2022-12-26 17:09:25 +0300

    extend card bundle

--- source/card_bundle.py
+++ source/card_bundle.py
@@ -15,3 +15,15 @@ class RushAttack(Card):
     def __init__(self):
         super(RushAttack, self).__init__(NoneSprite(), 'Влететь!', 'Влетает во врага и наносит ему 10 урона',
                                          CardType.Attack, ActionAreaType.OneEnemy, lambda y, x: x.apply_damage(10))
+
+
+class FastPunch(Card):
+    def __init__(self):
+        super(FastPunch, self).__init__(NoneSprite(), 'быстрый удар', 'сносит врага на X хп', CardType.Attack,
+                                        ActionAreaType.OneEnemy, lambda y, x: x.apply_damage(y.attack * 1))
+
+
+class ShieldRestruct(Card):
+    def __init__(self):
+        super(ShieldRestruct, self).__init__(NoneSprite(), 'пересборка', 'подзарежает щит на 10', CardType.Defend,
+                                             ActionAreaType.SelfAction, lambda y, x: x.apply_shield(10))
