da8ac8d6a51f37fb69ce288f097a9fcc1a7c1527
Horlou <egorchernov37@yandex.ru>
2022-12-26 17:56:19 +0300

    added battle screen

--- source/game_screen.py
+++ source/game_screen.py
@@ -3,6 +3,8 @@ import typing
 from source.player_view_map import PlayerViewMap
 from source.cell import CellModifierType
 from source.generate_mod import GenerateMod, GenerateModType
+from source.battle import Battle
+from source.enemies import Beetle
 
 
 class GameScreen(pygame.Surface):
@@ -17,7 +19,7 @@ class GameMapScreen(GameScreen):
     def __init__(self, size: typing.Tuple[int, int]):
         super(GameMapScreen, self).__init__(size)
 
-        self._player_view_map = PlayerViewMap(pygame.rect.Rect(0, 0, *size), None)
+        self._player_view_map = PlayerViewMap(pygame.Rect(0, 0, *size), None)
         cell_dict = {CellModifierType.EmptyCell: GenerateMod(GenerateModType.Base, 1),
                      CellModifierType.EnemyCell: GenerateMod(GenerateModType.Probability, 1)}
         self._player_view_map.generate_map((50, 50), cell_dict)
@@ -28,3 +30,19 @@ class GameMapScreen(GameScreen):
     @property
     def game_map(self):
         return self._player_view_map
+
+
+class BattleScreen(GameScreen):
+    def __init__(self, size: typing.Tuple[int, int], player_entities: typing.List):
+        super(BattleScreen, self).__init__(size)
+
+        self._player_entities = player_entities
+        enemy_entities = [Beetle(), Beetle(), Beetle()]
+        self._battle = Battle(pygame.Rect(0, 0, *size), player_entities, enemy_entities)
+
+    def draw(self, screen: pygame.Surface):
+        self._battle.draw(screen)
+
+    @property
+    def battle(self):
+        return self._battle
