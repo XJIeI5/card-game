4998bb361eb13568ea15d80c8bcdb2201dbcada8
Horlou <egorchernov37@yandex.ru>
2023-01-02 18:07:21 +0300

    ui extend: accept dialog added

--- source/ui.py
+++ source/ui.py
@@ -1,6 +1,7 @@
 import pygame
 import typing
 from enum import Enum
+from source.data.sprites.primitives import GreenBackgroundSprite, RedBackgroundSprite, GrayBackgroundSprite
 
 
 class Alignment(Enum):
@@ -14,14 +15,13 @@ class Label(pygame.sprite.Sprite):
                  alignment=Alignment.Center):
         super(Label, self).__init__()
         self.image = pygame.transform.scale(image, size)
-        self._rect = self.image.get_rect()
+        self.rect = self.image.get_rect()
         self._text = text
         self._font_size = font_size
-        self._offset = (0, 0)
         self._alignment = alignment
 
     def draw(self, screen: pygame.Surface, position: typing.Tuple[int, int]):
-        self._offset = position
+        self.rect.x, self.rect.y = position
         screen.blit(self.image, position)
         text = pygame.font.Font(None, self._font_size).render(self._text, True, pygame.Color('white'))
         if self._alignment == Alignment.Center:
@@ -37,12 +37,54 @@ class Label(pygame.sprite.Sprite):
     def set_text(self, new_text: str):
         self._text = new_text
 
-    @property
-    def rect(self):
-        return pygame.Rect(self._rect.x + self._offset[0],
-                           self._rect.y + self._offset[1],
-                           self._rect.width, self._rect.height)
+    def set_font_size(self, new_font_size: int):
+        self._font_size = new_font_size
+
+
+class AcceptDialog(pygame.sprite.Sprite):
+    def __init__(self, image: pygame.Surface, size: typing.Tuple[int, int], title: str = '', text: str = '',
+                 font_size: int = 12, info_font_size: int = 12, alignment: Alignment = Alignment.Center):
+        super(AcceptDialog, self).__init__()
+        self.image = pygame.transform.scale(image, size)
+        self.rect = self.image.get_rect()
+        self._title = title
+        self._text = text
+        self._font_size = font_size
+        self._info_font_size = info_font_size
+        self._alignment = alignment
+
+        self._accept_button = Label(GreenBackgroundSprite().image, (self.rect.width // 2, self.rect.height // 10),
+                                    text='OK', font_size=self._font_size)
+        self._reject_button = Label(RedBackgroundSprite().image, (self.rect.width // 2, self.rect.height // 10),
+                                    text='не ОК', font_size=self._font_size)
+
+    def draw(self, screen: pygame.Surface, position: typing.Tuple[int, int]):
+        print(self.rect.height)
+        self.rect.x, self.rect.y = position
+
+        title_label = Label(GrayBackgroundSprite().image, (self.rect.width, self.rect.height // 10),
+                            text=self._title, font_size=self._font_size)
+        title_label.draw(screen, self.rect.topleft)
+
+        info_label = Label(self.image, (self.rect.width, self.rect.height - self.rect.height // 5),
+                           text=self._text, font_size=self._info_font_size)
+        info_label.draw(screen, (self.rect.x, self.rect.y + title_label.rect.height))
+
+        self._accept_button.draw(screen, (self.rect.width,
+                                          self.rect.y + self.rect.height - self._accept_button.rect.height))
+        self._reject_button.draw(screen, (self.rect.width // 2,
+                                          self.rect.y + self.rect.height - self._reject_button.rect.height))
+
+    def set_text(self, new_text: str):
+        self._text = new_text
+
+    def set_font_size(self, new_font_size: int):
+        self._font_size = new_font_size
 
     @property
-    def offset(self):
-        return self._offset
+    def accept_button(self):
+        return self._accept_button
+
+    @property
+    def reject_button(self):
+        return self._reject_button
