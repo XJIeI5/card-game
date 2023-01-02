2bf3e8f624a4a679f5b02f59e16823ec21c5029a
Horlou <egorchernov37@yandex.ru>
2023-01-02 18:13:51 +0300

    added item_type

--- source/items_bundle.py
+++ source/items_bundle.py
@@ -1,12 +1,12 @@
 from source.data.sprites.primitives import BlueBackgroundSprite, GreenBackgroundSprite
-from source.item import Item
+from source.item import Item, ItemType
 
 
 class RockItem(Item):
     def __init__(self):
-        super(RockItem, self).__init__(BlueBackgroundSprite(), 'камень', 64)
+        super(RockItem, self).__init__(BlueBackgroundSprite(), 'камень', 64, ItemType.Collectable)
 
 
 class GlassItem(Item):
     def __init__(self):
-        super(GlassItem, self).__init__(GreenBackgroundSprite(), 'стекло', 2)
+        super(GlassItem, self).__init__(GreenBackgroundSprite(), 'стекло', 2, ItemType.Collectable)
