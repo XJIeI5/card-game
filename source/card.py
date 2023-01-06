ec1c367b87dcd01cb36be25cc89acbfb04e33333
Horlou <egorchernov37@yandex.ru>
2023-01-06 16:26:46 +0300

    drawing card description bugfix

--- source/card.py
+++ source/card.py
@@ -1,24 +1,7 @@
 import pygame
 import typing
 from enum import Enum
-
-
-def blit_text(screen, text, pos, font, color=pygame.Color('black')):
-    words = [word.split(' ') for word in text.splitlines()]  # 2D array where each row is a list of words.
-    space = font.size(' ')[0]  # The width of a space.
-    max_width, max_height = screen.get_size()
-    x, y = pos
-    for line in words:
-        for word in line:
-            word_surface = font.render(word, 0, color)
-            word_width, word_height = word_surface.get_size()
-            if x + word_width >= max_width:
-                x = pos[0]  # Reset the x.
-                y += word_height  # Start on new row.
-            screen.blit(word_surface, (x, y))
-            x += word_width + space
-        x = pos[0]  # Reset the x.
-        y += word_height  # Start on new row.
+from source.ui import blit_text, Alignment
 
 
 class CardType(Enum):
@@ -69,7 +52,8 @@ class Card(pygame.sprite.Sprite):
 
         surface.blit(self._mini_image, (15, 40))
 
-        blit_text(surface, self._description, (7, 120), pygame.font.Font(None, 18))
+        blit_text(surface, pygame.Rect(5, self._mini_image.get_size()[1], *surface.get_size()),
+                  self._description, 18, alignment=Alignment.Left)
 
         return surface
 
