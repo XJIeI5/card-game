2ecc3ddad0d6973c3ac1638a8bb44fcbadc38030
Horlou <egorchernov37@yandex.ru>
2023-01-06 16:27:45 +0300

    weapons added to items_bundle

--- source/items_bundle.py
+++ source/items_bundle.py
@@ -1,5 +1,6 @@
 from source.data.sprites.primitives import BlueBackgroundSprite, GreenBackgroundSprite, RedBackgroundSprite
 from source.item import Item, ItemType
+from source import card_bundle
 
 
 class RockItem(Item):
@@ -16,3 +17,11 @@ class HealingSerumItem(Item):
     def __init__(self):
         super(HealingSerumItem, self).__init__(RedBackgroundSprite(), 'сыворотка лечения', 10, ItemType.Consumable,
                                                action=lambda x: x.apply_hp(10))
+
+
+class SmallPistolItem(Item):
+    def __init__(self):
+        super(SmallPistolItem, self).__init__(GreenBackgroundSprite(), 'маленький пистолет', 1,
+                                              ItemType.Equipment.value.MainWeapon,
+                                              action=lambda x: x.extend_cards([card_bundle.FastShoot] * 2),
+                                              undo_action=lambda x: x.remove_cards([card_bundle.FastShoot] * 2))
