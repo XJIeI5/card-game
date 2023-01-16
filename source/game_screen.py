91953501c7a0c51e664c7beb4f368af18d726f4f
Horlou <egorchernov37@yandex.ru>
2023-01-16 18:12:50 +0300

    added home_screen and reformed battle_screen

--- source/game_screen.py
+++ source/game_screen.py
@@ -1,22 +1,24 @@
 import pygame
 import typing
+import random
 from source.player_view_map import PlayerViewMap
 from source.cell import CellModifierType
 from source.generate_mod import GenerateMod, GenerateModType
 from source.battle import Battle
-from source.enemies import Beetle
+from source.enemies_bundle import Beetle, Hover
 from source.ui import Label, Alignment
 from source.inventory import Inventory
 from source.palmtop_ui import PalmtopUI
 from source.planet_choose import PlanetChoose
 from source.hub import Hub
-from source.data.sprites.primitives import BlueBackgroundSprite
+from source.data.sprites.primitives import BlueBackgroundSprite, NoneSprite
 from source.store import Store, Money
 
 
 class GameScreen(pygame.Surface):
     def __init__(self, size: typing.Tuple[int, int]):
         super(GameScreen, self).__init__(size)
+        self._size = size
 
     def draw(self, screen: pygame.Surface):
         pass
@@ -67,12 +69,16 @@ class GameMapScreen(GameScreen):
 
 
 class BattleScreen(GameScreen):
-    def __init__(self, size: typing.Tuple[int, int], player_entities: typing.List):
+    def __init__(self, size: typing.Tuple[int, int], player_entities: typing.List, inventory: Inventory,
+                 planet_index: int):
         super(BattleScreen, self).__init__(size)
 
         self._player_entities = player_entities
-        enemy_entities = [Beetle(1), Beetle(1), Beetle(1)]
-        self._battle = Battle(pygame.Rect(0, 0, *size), player_entities, enemy_entities)
+        if planet_index == 0:
+            enemy_entities = [Beetle(1) for _ in range(random.randint(1, 2))]
+        else:
+            enemy_entities = [Hover(2) for _ in range(random.randint(1, 3))]
+        self._battle = Battle(pygame.Rect(0, 0, *size), player_entities, enemy_entities, inventory)
 
     def draw(self, screen: pygame.Surface):
         self._battle.draw(screen)
@@ -101,10 +107,11 @@ class PalmtopUIScreen(GameScreen):
 
 
 class PlanetChooseScreen(GameScreen):
-    def __init__(self, size: typing.Tuple[int, int]):
+    def __init__(self, size: typing.Tuple[int, int], money: Money):
         super(PlanetChooseScreen, self).__init__(size)
 
-        self._planet_choose = PlanetChoose(pygame.Rect(0, 0, *size))
+        self._planet_choose = PlanetChoose(pygame.Rect(0, 0, *size), money)
+        self._planet_choose.load_maps('./source/data/save')
 
     def draw(self, screen: pygame.Surface):
         self._planet_choose.draw(screen)
@@ -133,10 +140,11 @@ class HubScreen(GameScreen):
 
 
 class StoreScreen(GameScreen):
-    def __init__(self, size: typing.Tuple[int, int], money: Money):
+    def __init__(self, size: typing.Tuple[int, int], money: Money, inventory: Inventory):
         super(StoreScreen, self).__init__(size)
 
-        self._store = Store(pygame.rect.Rect(0, 0, *size), 5, 5, 2, money)
+        print(inventory.items)
+        self._store = Store(pygame.rect.Rect(0, 0, *size), 5, 5, 2, money, inventory)
 
     def draw(self, screen: pygame.Surface):
         self._store.draw(screen)
@@ -148,3 +156,12 @@ class StoreScreen(GameScreen):
     @property
     def exit_button(self):
         return self._store.exit_button
+
+
+class HomeScreen(GameScreen):
+    def __init__(self, size: typing.Tuple[int, int]):
+        super(HomeScreen, self).__init__(size)
+
+    def draw(self, screen: pygame.Surface):
+        label = Label(NoneSprite().image, (self._size[0] // 2, 50), text='нажмите на любую кнопку', font_size=36)
+        label.draw(screen, (self._size[0] // 4, self._size[1] - label.rect.height))
